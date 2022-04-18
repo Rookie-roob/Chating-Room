60e27b269e0c78965cdb5596d7a16876c092a35f
Klylee <2171869411@qq.com>
2022-04-18 23:39:50 +0800

    Create Account.h

---
+++ Account.h
@@ -0,0 +1,42 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+class Account
+{
+private:
+    unsigned int id;
+    unsigned int password;
+    char nick[20];
+    bool online;
+    std::vector<unsigned int> friendList;
+public:
+    Account(unsigned int id,unsigned int psw,char *nick,bool online=false):
+            id(id),password(psw),online(online)
+    {
+        strcpy(this->nick,nick);
+    }
+    ~Account();
+    inline unsigned int getID(){return this->id;}
+    inline char* getNickname(){return this->nick;}
+    inline bool checkPassword(unsigned int psw){return (psw==this->password);}
+    inline bool changeState(){online= !online; return online;}
+    inline bool showState(){return online;}
+    inline unsigned int getFriendnum(){return (unsigned int)friendList.size();}
+    void loadFriendList(unsigned int friendid); //use this function to load the information of friend list from data file
+    inline unsigned int getFriendID(int key){return friendList[key];}
+};
+
+Account::~Account()
+{
+}
+
+void Account::loadFriendList(unsigned int friendid)
+{
+    friendList.push_back(friendid);
+}
+
+
+#endif
